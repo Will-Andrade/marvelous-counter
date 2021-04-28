366dd3831b22f25a344bedef00dd0f5fabcd1294
Will-Andrade <willian.santos.andra@outlook.com>
2021-04-28 19:42:32 -0400

    added a function for coloring the counter

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const counterSpan = document.querySelector('span#value');
 const buttonContainer = document.querySelector('section.button-container');
+let counter = 0;
 
-buttonContainer.addEventListener('click', event => {
-    const arrayOfElementClasses = Array.from(event.target.classList);
-
-    if (arrayOfElementClasses.includes('decrease')) {
-        return counterSpan.textContent--
+const counterColorFeedback = counter => {
+    if (counter > 0) {
+        return counterSpan.style.color = 'green';
     }
 
-    if (arrayOfElementClasses.includes('reset')) {
-        return counterSpan.textContent = 0;
+    if (counter < 0) {
+        return counterSpan.style.color = 'red';
     }
 
-    if (arrayOfElementClasses.includes('increase')) {
-        return counterSpan.textContent++
+    return counterSpan.style.color = 'black';
+}
+
+const handleCounter = ({ target }) => {
+    if (target.classList.contains('decrease')) {
+        counterSpan.textContent = --counter;
+        return counterColorFeedback(counter)
     }
-})
+
+    if (target.classList.contains('increase')) {
+        counterSpan.textContent = ++counter
+        return counterColorFeedback(counter)
+    }
+    
+    counter = 0
+    counterSpan.textContent = counter;
+    return counterColorFeedback(counter)
+}
+
+buttonContainer.addEventListener('click', handleCounter)
